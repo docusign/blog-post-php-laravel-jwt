f882dce209ac40024e4309d312c15170fa053725
ivdinkovds <74546686+ivdinkovds@users.noreply.github.com>
2022-06-07 17:02:09 +0100

    Create response.blade.php

---
+++ response.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Creating DocuSign contract</title>
+</head>
+    <body>
+    <div class="container my-5">
+        <div class="mx-auto" style="width:50%">           
+                <pre>{{ $result }}</pre>
+        </div>
+    </div>
+    </body>
+</html>
